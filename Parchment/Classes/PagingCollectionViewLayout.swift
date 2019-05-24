001750e0fb5eb6a8b24efbc2fdd5e85e7b3c907f
lianleven <lianleven@163.com>
2019-05-24 11:20:12 +0800

    indicatorInsets handle

--- Parchment/Classes/PagingCollectionViewLayout.swift
+++ Parchment/Classes/PagingCollectionViewLayout.swift
@@ -436,13 +436,7 @@ open class PagingCollectionViewLayout<T: PagingItem>:
   
   private func indicatorInsetsForIndex(_ index: Int) -> PagingIndicatorMetric.Inset {
     if case let .visible(_, _, _, insets) = options.indicatorOptions {
-      if index == 0 && range.upperBound == 1 {
-        return .both(insets.left, insets.right)
-      } else if index == range.lowerBound {
-        return .left(insets.left)
-      } else if index >= range.upperBound - 1 {
-        return .right(insets.right)
-      }
+      return .both(insets.left, insets.right)
     }
     return .none
   }
